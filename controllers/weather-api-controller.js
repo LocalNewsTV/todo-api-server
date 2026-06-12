9de8f8b043401dda952a666b2fdf0b66eafb5349
Matthew Logan <loganmatthewdev@gmail.com>
2026-06-12 15:22:19 -0700

    Update packages, remove axios

--- controllers/weather-api-controller.js
+++ controllers/weather-api-controller.js
@@ -1,13 +1,16 @@
 import dotenv from 'dotenv';
-import axios from 'axios';
 dotenv.config();
 
 export const fetchWeather = async (req, res) => {
   try {
-    const weatherData = await axios.get(`https://api.weatherapi.com/v1/current.json?key=${process.env.WEATHER_API_KEY}&q=${req.body.location}&aqi=no`);
-    res.status(200).send(JSON.stringify(weatherData.data));
-  } catch(ex) {
+    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${process.env.WEATHER_API_KEY}&q=${req.body.location}&aqi=no`);
+    if (response.ok) {
+      const data = await response.json()
+      return res.status(200).send(JSON.stringify(data));
+    }
+    return res.status(500).send("Server Error Occurred")
+  } catch (ex) {
     console.log(ex);
-    res.status(400).send("Bad Request");
+    return res.status(400).send("Bad Request");
   }
 }
